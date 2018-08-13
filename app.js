f1cd0e0fd6560aa4657bfbe94b60fbf7f5c52798
yaphtes <yaphtes.ks@gmail.com>
2018-08-13 21:56:54 +0400

    added code

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const path = require('path');
 const mongoose = require('mongoose');
-const WebSocket = require('ws');
 const app = express();
 const serverPort = 8000;
-const wssPort = 8080;
 
 
 app.use(cors());
-// app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 app.use(express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -26,12 +23,8 @@ const db = mongoose.connection;
 db.on('error', () => console.log('mongodb error'));
 db.once('open', () => {
   console.log('mongodb started');
-  const wss = new WebSocket.Server({ port: wssPort }, () => {
-    console.log('wss started');
-    app.set('wss', wss);
-    require('./routes');
-    app.listen(serverPort, () => console.log('server started'));
-  });
+  require('./routes');
+  app.listen(serverPort, () => console.log('server started'));
 });
 
 module.exports = { app, db };
